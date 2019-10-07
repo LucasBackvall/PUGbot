56ed72c2c1ffbe7cb8c7bf773cbe156ff47c4a3d
Glitch (pugbot) <none>
2019-10-07 10:35:04 +0000

    Glitch version

--- index.js
+++ index.js
@@ -1,39 +1,390 @@
-const Discord = require('discord.js');
+const util = require("util");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const auth = require('./auth.json');
+const fs = require('fs');
+const pugs = require('./pugs.json');
 
-client.once('ready', () => {
-	console.log('Ready!');
+// Authentication
+try{
+  const auth = require("./auth.json")
+  client.login(auth.token);
+  console.log("Authenticated.");
+} catch (err) {
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  readline.question("What is your bot token?\n", token => {
+    client.login(token);
+    console.log("Authenticated.");
+    readline.close();
+  });
+}
+
+const prefix = ".";
+const colums = false;
+
+var ch = ".."
+var link = "Please wait, the bot hasn't found the link yet."
+var last = "The bot hasn't had time to load the last PUG yet."
+var players = [];
+for (var i = 0; i < pugs.length; i++) {
+  players.push(new Array());
+}
+
+function printPUG(i) {
+  var msg = pugs[i].name
+  while (msg.length < 3) { msg += " ";}
+  // \uD83C\uDF00 
+  msg += " (" + players[i].length + "/" + pugs[i].max + "): ";
+  var first = true;
+  players[i].forEach(function (player) {
+    if (first) {msg += player.username;first=false;}
+    else {msg += ", " + player.username;}
+  });
+  return msg;
+}
+
+
+function getUser(mention) {
+	if (!mention) return;
+	if (mention.startsWith('<@') && mention.endsWith('>')) {
+		mention = mention.slice(2, -1);
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+		return client.users.get(mention);
+  }
+}
+
+
+function fromPlayers(lists) {
+  var out = [];
+  lists.forEach(function (list) {
+    var olist = [];
+    list.forEach(function (player) {
+      olist.push(player.id);
+    });
+    out.push(olist);
+  });
+  return out;
+}
+
+function toPlayers(lists) {
+  var out = [];
+  lists.forEach(function (list) {
+    var olist = [];
+    list.forEach(function (id) {
+      olist.push(client.users.get(id));
+    });
+    out.push(olist);
+  });
+  return out;
+}
+
+
+function removePlayer(i, name) {
+  if (players[i] == undefined) {return;}
+  var j = players[i].indexOf(name)
+  if (j != -1) {
+    players[i].splice(j, 1);
+  }
+}
+
+
+function removePlayerAll(name) {
+  for (var i = 0; i < pugs.length; i++) {
+    removePlayer(i, name);
+  }
+}
+
+
+function listactive() {
+  var anyone = false;
+  for (var i = 0; i < players.length; i++) {
+    if (players[i].length > 0) {anyone = true;}
+  }
+  if (!anyone) {
+    return "There is currently nobody registered for a PUG.";
+  }
+  var msg = "```fix\nAll PUGs with registered players:\n";
+  for (var i = 0; i < pugs.length; i++) {
+    if (players[i].length > 0) {
+      msg += "\n" + printPUG(i);
+    }
+  }
+  return msg + "```"
+}
+
+
+function updateLast(players, pug) {
+  last = "Last game:\n\n**"+pug.name+"** with: ";
+  var first = true;
+  players.forEach(function (player) {
+    if (first) {last += "**"+player.username+"**";first=false;}
+    else {last += ", " + "**"+player.username+"**";;}
+  });
+}
+
+function writeData() {
+  try {
+    let data = {players: fromPlayers(players), last: last, invite: link, channel: ch}
+    fs.writeFile('data.json', JSON.stringify(data), (err) => {
+      if (err) throw err;
+      console.log("Write successfull.");
+    });
+  } catch (err) {
+    console.log("Unable to write data to 'data.json'\n\n" + err);
+  }
+}
+
+
+client.once("ready", () => {
+  try {
+    fs.readFile('data.json', (err, d) => {
+      if (err) throw err;
+      let data = JSON.parse(d);
+      last = data.last;
+      players = toPlayers(data.players);
+      link = data.invite;
+      ch = data.channel;
+    });
+  } catch (err) {
+    console.log("Failed to load data form './data.json'\n\n" + err);
+    last = "Unable to load last PUG. This is because the data-file couldn't be read";
+    link = "Unable to load invite link. This is because the data-file couldn't be read";
+    ch = ".";
+  }
+  console.log("Bot started.");
 });
-client.login(auth.token);
 
-
-const prefix = '.';
-
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	switch (command) {
-		case 'help':
-			message.channel.send("No help available yet.");
-			break;
-		case 'testargs':
-			if (!args.length) {
-				return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-			} else {
-				message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-			}
-			break;
-		case 'admincheck':
-			if (message.member.roles.some(role => role.name === 'PUGadmin')) {
-				message.channel.send("You're a PUG-admin.");
-			} else {
-				message.channel.send("You're not a PUG-admin.");
-			}
-			break;
-	}
+client.on("message", message => {
+  try{
+    // Disallow commands outside of specified channel. DM commands are also disallowed so you can't enter a list without people seeing it.
+    // TODO: Allow some commands in DM. If a report system is implemented you should be able to report players in DM to bot.
+    if (message.channel.type != "text" || ch != "." && ch != message.channel.name) return;
+    // Ignore messages that arent commands or that's recieved from bots
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // Ignore messages that's recieved from banned players
+    if (message.member.roles.some(role => role.name === "PUGbanned")) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+    var sender = message.author;
+    console.log(sender.username + ": " + command +" ("+ args + ")")
+  
+  
+    switch (command) {
+      
+      // Display helpmessage
+      case "help":
+        var rich = new Discord.RichEmbed()
+          .setColor("#FA6607")
+          .setTitle("Available commands:")
+          .addField(prefix + "help", "Show this message.", colums)
+          .addField(prefix + "list", prefix + "ls for short. Show all PUGs that you can join.", colums)
+          .addField(prefix + "listactive", prefix + "lsa for short. Show all PUGs you can join that already have registered players.", colums)
+          .addField(prefix + "last", "Show last filled PUG with it's players.", colums)
+          .addField(prefix + "join <PUGs>", prefix + "j for short. Join one or several PUGs. For example: both '.join 2v2 3v3' and '.j 2v2 3v3' would join both 2v2 and 3v3.", colums)
+          .addField(prefix + "leave <PUGs>", prefix + "lv for short. Leave one or several PUGs. For example both '.leave 2v2 3v3' and '.lv 2v2 3v3' would leave both 2v2 and 3v3.", colums)
+          .addField(prefix + "leaveall", prefix + "lva for short. Leave all PUGs you're registered to.", colums)
+          .addField(prefix + "invite", prefix + "inv for short. Get the invite link sent in chat to invite your friends to this community!", colums)
+          .addField(prefix + "promote (PUGs)", prefix +"p for short. Promote one or more PUGs. If you don't specify any PUGs all PUGs with registered players will be promoted.")
+        message.channel.send(rich);
+        if (message.member.roles.some(role => role.name === "PUGadmin")) {
+          var rich = new Discord.RichEmbed()
+            .setColor("#FA6607")
+            .setTitle("Available administrator commands:")
+            .addField(prefix + "reset", "Resets all PUGs.", colums)
+            .addField(prefix + "adminadd <user> <PUGs>", "Add user to one or several PUGs. For example: '.adminadd @LurreB 2v2 3v3'", colums)
+            .addField(prefix + "adminremove <user> <PUGs>", "Remove user from one or several PUGs. For example: '.adminremove @LurreB 2v2 3v3'", colums)
+            .addField(prefix + "adminremoveall <user>", "Remove user from all PUGs. For example: '.adminremoveall @LurreB'", colums)
+            .addField(prefix + "setchannel", "Sets the current channel as the channel the bot will listen to and respond in.", colums)
+            .addField(prefix + "setinvite <link>", "Set the new invite link.", colums)
+          message.author.createDM().then(function(channel) {
+            channel.send(rich);
+          }, function(err) {
+            console.log("Couldn't send admincommands as DM.\n\n" + err)
+            message.channel.send(rich);
+          });
+        }
+        break;
+      
+      
+      case "invite":
+      case "inv":
+        message.channel.send()
+        break;
+      
+      
+      case "setinvite":
+        link = args[0];
+        writeData();
+        break;
+      
+    
+      // Reset PUGs (default is all PUGs)
+      case "reset":
+        if (!message.member.roles.some(role => role.name === "PUGadmin")) {message.channel.send("Only PUGadmins can perform this command.");break;}
+        if(args == undefined) {
+           for (var i = 0; i < pugs.length; i++) {
+            players[i] = new Array();
+          }
+          message.channel.send("All PUGs reset!");
+        } else {
+          args.forEach(function(arg) {
+            for (var i = 0; i < pugs.length; i++) {
+              if (pugs[i].name == arg) {
+                message.channel.send("**" +pugs[i].name+ "** reset!");
+                players[i] = new Array();
+              }
+            }
+          });
+        }
+        writeData();
+        break;
+  
+    
+      // Set current channel to the operating channel of the bot.
+      case "setchannel":
+        if (!message.member.roles.some(role => role.name === "PUGadmin")) {message.channel.send("Only PUGadmins can perform this command.");break;}
+        ch = message.channel.name;
+        writeData();
+        break;
+      
+    
+      // Add another user to PUG
+      case "adminadd":
+        if (!message.member.roles.some(role => role.name === "PUGadmin")) {message.channel.send("Only PUGadmins can perform this command.");break;}
+        sender = getUser(args[0]);
+        args.splice(0, 1);
+        try {
+          var test = sender.username;
+        } catch (err) {
+          message.channel.send("Tried to add non-user to PUG.");
+          break;
+        }
+        
+      
+      // Join a PUG
+      case "j":
+        var lfg = true;
+        // For each PUG
+        for (var i = 0; i < pugs.length; i++) {
+          // If PUG is in message argument and we're still looking for a group (lfg)
+          //   If a player finds a game, they're no longer looking for a group (lfg)
+          if (args.indexOf(pugs[i].name) != -1 && lfg) {
+            // If player isn't in PUG
+            if (players[i].indexOf(sender) == -1) {
+              // If that PUG is now full (push returns new length of array)
+              if (players[i].push(sender) >= pugs[i].max) {
+                lfg = false; // Not looking for a group anymore
+                updateLast(players[i], pugs[i]);
+                var msg = "Found a game!\n\n**"+pugs[i].name+"** is filled! All players (**"+players[i]+"**) please join a voice channel together.";
+                msg += "\n\nYou have all been automatically removed from all other playlists."
+                message.channel.send(msg);
+                players[i].forEach(function (participant) {
+                  participant.createDM().then(function(channel) {
+                    channel.send(msg);
+                  }, function(err) {
+                    console.log("Couldn't send found game DMs.")
+                  });
+                  removePlayerAll(participant);
+                  removePlayerAll(sender); // Voodoo shit, won't remove the sender for some unknown reason.ll
+                });
+                break;
+              }
+            // If player already in PUG
+            } else {
+              message.channel.send("You're already in list **" + pugs[i].name + "**");
+            }
+          }
+        }
+        if (lfg) {
+          message.channel.send(listactive());
+        }
+        writeData();
+        break;
+      
+      
+      // Remove another user from all PUGs
+      case "adminremoveall":
+        if (!message.member.roles.some(role => role.name === "PUGadmin")) {message.channel.send("Only PUGadmins can perform this command.");break;}
+        sender = getUser(args[0]);
+    
+      
+      // Leave all PUGs
+      case "lva":
+        removePlayerAll(sender);
+        //message.channel.send("Removed **" + sender + "** from all lists.");
+        message.channel.send(listactive());
+        writeData();
+        break;
+    
+      
+      // Remove another user from a PUG
+      case "adminremove":
+        if (!message.member.roles.some(role => role.name === "PUGadmin")) {message.channel.send("Only PUGadmins can perform this command.");break;}
+        sender = getUser(args[0]);
+        args.splice(0, 1);
+    
+      
+      // Leave a PUG
+      case "leave":
+      case "lv":
+        args.forEach(function (pugname) {
+          for (var i = 0; i < pugs.length; i++) {
+            if (pugs[i].name == pugname) {
+              removePlayer(i, sender);
+              //message.channel.send("Removed **" + sender + "** from **" + pugname + "**");
+            }
+          }
+          console.log("\n\n");
+        });
+        message.channel.send(listactive());
+        writeData();
+        break;
+      
+      
+      case "promote":
+      case "p":
+        if (args.length == 0) message.channel.send("@here\n" + listactive())
+        else {
+          var msg = "@here\n```fix\nFollowing PUG(s) were promoted by "+message.author.username+"\n";
+          args.forEach(function (arg) {
+            for (var i = 0; i < pugs.length; i++) {
+              if (pugs[i].name == arg) msg += "\n" + printPUG(i)
+            }
+          });
+          msg += "```";
+          message.channel.send(msg);
+        }
+        break;
+      
+      
+      // List all PUGs with players registered
+      case "listactive":
+      case "lsa":
+        message.channel.send(listactive());
+        break;
+      
+      
+      // List all PUGs
+      case "list":
+      case "ls":
+        var msg = "```fix\nAll PUGs:\n";
+        for (var i = 0; i < pugs.length; i++) {
+          msg += "\n" + printPUG(i);
+        }
+        message.channel.send(msg + "```");
+        break;
+    
+    
+      // Repeat last PUG
+      case "last":
+        message.channel.send(last);
+        break;
+    }
+  } catch (err) {
+    console.log("Error parsing this command.\n\n" + err);
+  }
 });
-
